f69f4cbd39444a09ecdde339fd380b5cfc1755d1
Jaume Singla <jaume.singlavalls@gmail.com>
2016-01-22 18:51:32 +0100

    Added restrictions with permissions to the endpoints

--- team_passwords/rest/views.py
+++ team_passwords/rest/views.py
@@ -5,11 +5,36 @@ __author__ = 'dracks'
 
 from team_passwords.models import Site, Group, GroupUserPermission
 from team_passwords.rest.serializers import SiteSerializer, GroupSerializer, GroupUserPermissionSerializer
-from team_passwords.permissions import GroupHasPermissions, SiteHasPermissions, get_group_permissions
+from team_passwords.permissions import GroupHasPermissions, SiteHasPermissions, get_group_permissions, \
+    GroupUserPermissions
 from rest_framework import viewsets
 from rest_framework.response import Response
 from oauth2_provider.ext.rest_framework import permissions
 
+
+def check_create_group(field, permission):
+    def f(func):
+        def func_call(self, request, *args, **kwargs):
+            group_pk = request.data[field]
+            group_permission = get_group_permissions(request.user, Group.objects.get(pk=group_pk))
+            if group_permission >= permission:
+                return func(self, request, *args, **kwargs)
+            self.permission_denied(request)
+
+        return func_call
+
+    return f
+
+"""
+{
+    "group": 24,
+    "name": "test 1",
+    "description": "description",
+    "user": "1",
+    "password": "1",
+    "url": "1"
+}
+"""
 
 class SiteViewSet(viewsets.ModelViewSet):
     queryset = Site.objects.all()
@@ -22,6 +47,14 @@ class SiteViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         groups = GroupUserPermission.get_all_groups_read(self.request.user)
         return Site.objects.filter(group__in=groups)
+
+    @check_create_group('group', 2)
+    def create(self, request, *args, **kwargs):
+        return super(SiteViewSet, self).create(request, *args, **kwargs)
+
+    @check_create_group('group', 2)
+    def update(self, request, *args, **kwargs):
+        return super(SiteViewSet, self).update(request, *args, **kwargs)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -60,10 +93,26 @@ class GroupViewSet(viewsets.ModelViewSet):
             else:
                 return GroupUserPermission.get_parent_groups_read(self.request.user)
 
+    @check_create_group('parent', 3)
+    def create(self, request, *args, **kwargs):
+        return super(GroupViewSet, self).create(request, *args, **kwargs)
+
+    @check_create_group('parent', 3)
+    def update(self, request, *args, **kwargs):
+        return super(GroupViewSet, self).update(request, *args, **kwargs)
+
 
 class GroupUserPermissionViewSet(viewsets.ModelViewSet):
     queryset = GroupUserPermission.objects.all()
     serializer_class = GroupUserPermissionSerializer
+    permission_classes = (GroupUserPermissions, )
     filter_backends = (filters.DjangoFilterBackend,)
     filter_fields = ('group', 'user')
 
+    @check_create_group('group', 3)
+    def create(self, request, *args, **kwargs):
+        return super(GroupUserPermissionViewSet, self).create(request, *args, **kwargs)
+
+    @check_create_group('group', 3)
+    def update(self, request, *args, **kwargs):
+        return super(GroupUserPermissionViewSet, self).update(request, *args, **kwargs)
